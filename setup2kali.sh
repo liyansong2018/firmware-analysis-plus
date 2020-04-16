b32c8d4984bbce8dd966df4c337304eee88b6833
liyansong2018 <1321993383@qq.com>
2020-04-16 15:10:02 +0800

    pip2

---
+++ setup2kali.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+sudo apt update
+sudo python get-pip.py
+sudo python3 get-pip.py
+sudo apt install -y python3-pexpect unzip busybox-static fakeroot kpartx snmp uml-utilities util-linux vlan qemu-system-arm qemu-system-mips qemu-system-x86 qemu-utils
+sudo pip2 install python-magic -i https://mirrors.aliyun.com/pypi/simple/ 
+
+cd firmadyne
+
+firmadyne_dir=$(realpath .)
+
+# Set FIRMWARE_DIR in firmadyne.config
+sed -i "/FIRMWARE_DIR=/c\FIRMWARE_DIR=$firmadyne_dir" firmadyne.config
+
+# Comment out psql -d firmware ... in getArch.sh
+sed -i 's/psql/#psql/' ./scripts/getArch.sh
+
+# Change interpretor in extractor.py to python3
+sed -i 's/env python/env python3/' ./sources/extractor/extractor.py
+cd ..
+
+echo "Setting up firmware analysis plus"
+chmod +x fat.py
+chmod +x reset.py
+
+# Set firmadyne_path in fat.config
+sed -i "/firmadyne_path=/c\firmadyne_path=$firmadyne_dir" fat.config
+
+echo "====================================================="
+echo "Firmware Analysis Plus installed successfully!"
+echo "Before running fat.py for the first time,"
+echo "please edit fat.config and provide your sudo password"
+echo "====================================================="
